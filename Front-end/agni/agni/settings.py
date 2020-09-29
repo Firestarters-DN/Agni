1efef6caecb4e95144d46821da90ccf08d9038f3
nuts-rice <red44443@gmail.com>
2020-09-29 10:02:35 -0400

    Removed Django secret key
    Find it in Secrets: only available to collabarators

--- Front-end/agni/agni/settings.py
+++ Front-end/agni/agni/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%gg=lj5@%r$1a#)my2seab)d)801lva@k6fy*oiw+#=*)&cmkb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
